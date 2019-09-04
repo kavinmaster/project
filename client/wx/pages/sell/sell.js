a37f6b35166f7406f2d056098bc0457c052a5c18
huimengxiang <1316403904@qq.com>
2019-09-04 17:27:34 +0800

    add sell

--- client/wx/pages/sell/sell.js
+++ client/wx/pages/sell/sell.js
@@ -5,62 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    windowHeight: null,
+    index:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        let windowHeight = (res.windowHeight * (750 / res.windowWidth)) - 200+ 'rpx';
 
+        that.setData({
+          windowHeight: windowHeight
+        });
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onclickTop: function (res) {
+    let id = res.currentTarget.dataset.change;
+    console.log(id);
+    this.setData({
+      index: id
+    });
   }
+ 
 })
