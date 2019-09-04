14da68e616b47aa9b2dda8ca7550816a33befd17
solitude <wx_bf2f1117a26a45a6a767d5bd839e9f0d@git.code.tencent.com>
2019-09-04 17:09:45 +0800

    this is indexView

--- client/wx/pages/index/index.js
+++ client/wx/pages/index/index.js
@@ -5,5 +5,33 @@ const app = getApp()
 Page({
   data: {
    
+  },
+  locationMap: function () {
+    wx.getLocation({ //获取当前经纬度
+      type: 'wgs84', //返回可以用于wx.openLocation的经纬度，官方提示bug: iOS 6.3.30 type 参数不生效，只会返回 wgs84 类型的坐标信息  
+      success: function (res) {
+        wx.openLocation({ //​使用微信内置地图查看位置。
+          latitude: 24.345765, //要去的纬度-地址
+          longitude: 102.565315, //要去的经度-地址
+          name: "万达影城(锦华路万达店)",
+          address: '锦华路一段68号万达广场3楼365号'
+        })
+      }
+    })
+  },
+  phoneCall: function () {
+    wx.makePhoneCall({
+      phoneNumber: '1340000' //仅为示例，并非真实的电话号码
+    })
+  },
+  toMovieView:function(){
+    wx.navigateTo({
+      url: '../movie/movie',
+    })
+  },
+  toSellView: function () {
+    wx.navigateTo({
+      url: '../sell/sell',
+    })
   }
 })
