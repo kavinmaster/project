fb38ebd3a652612fa3778bd7cfabccd0bac0028f
huimeng-s <3503254100@qq.com>
2019-09-05 15:01:10 +0800

    add

--- client/wx/pages/Details/Details.js
+++ client/wx/pages/Details/Details.js
@@ -9,104 +9,77 @@ Page({
       indexImg:0,
       topics:[
       {
+          id: 0,
+          name: "大卫▪雷奇",
+          play: "导演",
         src: "../../imgs/01.jpg"
       },
       {
+        id: 1,
+        name: "道恩▪强森",
+        play: "饰 霍布斯",
         src: "../../imgs/02.jpg"
       },
         {
+          id: 2,
+          name: "杰森▪斯坦森",
+          play: "饰 霍布斯",
           src: "../../imgs/03.jpg"
         },
         {
+          id: 3,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/04.jpg"
         },
         {
+          id: 4,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/05.png"
         },
         {
+          id: 5,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/06.png"
         },
         {
+          id: 6,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/07.png"
         },
         {
+          id: 7,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/08.png"
         },
         {
+          id: 7,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/09.png"
         },
         {
+          id: 7,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/10.png"
         },
         {
+          id: 7,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/11.png"
         },
         {
+          id: 7,
+          name: "大卫▪雷奇",
+          play: "饰 霍布斯",
           src: "../../imgs/12.png"
         }
-      ],
-      names:[
-
-        {
-          id:0,
-          name:"大卫▪雷奇"
-        },
-
-        {
-          id: 1,
-          name: "大卫▪雷奇"
-        },
-
-        {
-          id: 2,
-          name: "大卫▪雷奇"
-        },
-
-        {
-          id: 3,
-          name: "大卫▪雷奇"
-        },
-
-        {
-          id: 4,
-          name: "大卫▪雷奇"
-        },
-
-        {
-          id: 5,
-          name: "大卫▪雷奇"
-        },
-        {
-          id: 6,
-          name: "大卫▪雷奇"
-        },
-        {
-          id: 7,
-          name: "大卫▪雷奇"
-        },
-        {
-          id: 8,
-          name: "大卫▪雷奇"
-        },
-        {
-          id: 9,
-          name: "大卫▪雷奇"
-        },
-        {
-          id: 10,
-          name: "大卫▪雷奇"
-        },
-
-        {
-          id: 11,
-          name: "大卫▪雷奇"
-        },
-
-        {
-          id: 12,
-          name: "大卫▪雷奇"
-        },
-
       ],
 
     pictures:[
@@ -147,6 +120,14 @@ Page({
     this.setData({
       isFold: !this.data.isFold,
     })
-  }
+  },
+  clickTopItem: function (res) {
+    //获得传递过来的数据
+    let id = res.currentTarget.dataset.yjs;
+    //把index换成id
+    this.setData({
+      indexImg: id
+    });
+  },
 })
 
