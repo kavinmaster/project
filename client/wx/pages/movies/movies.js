b1a5e1da95a3f35f4eddfd667372b9f25fe1cfa5
wangjilin <1801978253@qq.com>
2019-09-11 14:03:59 +0800

    add movie

--- client/wx/pages/movies/movies.js
+++ client/wx/pages/movies/movies.js
@@ -5,23 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: ["	https://movie-1300072979.cos.ap-chengdu.myqcloud.com/movie_scroll2.png",
-    "	https://movie-1300072979.cos.ap-chengdu.myqcloud.com/movie_scroll1.png"
-    ]
+    imgUrls: [],
+    type:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this;
+    //获得后台服务的数据
+    wx.request({
+      url: 'http://localhost:8888/items',
+      success: function (res) {
+        if (res.statusCode == 200) {
+          console.log(res.data);
+          // let imgUrls= res.data.imgUrls;
+          let type = res.data;
+          that.setData({  type: type });
+        }
+      }
+    });
+    wx.request({
+      url: 'http://localhost:8888/comment',
+      success: function (res) {
+        if (res.statusCode == 200) {
+          console.log(res.data);
+          
+          let imgUrls= res.data;
+          that.setData({ imgUrls: imgUrls });
+        }
+      }
+    }); 
   },
-
-  toDetails: function(){
-    wx.navigateTo({
-      url: '../Details/Details',
+      toDetails: function () {
+        wx.navigateTo({
+          url: '../Details/Details',
+        })
+      }
     })
-  }
-
-  
-})
